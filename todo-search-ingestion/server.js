c6b59cf5656877bfa092bf9d69c5f1d7fc863b74
dbaker-alfresco <doug.baker@alfresco.com>
2020-05-01 17:28:07 +0100

    Added deployment of app to Elastic Beanstalk

--- todo-search-ingestion/server.js
+++ todo-search-ingestion/server.js
@@ -4,84 +4,103 @@ const envProps = require('./env_props');
 
 // Elasticsearch Client Setup //////////////////////////////////////////////////////////////////////////////////////////
 const elasticClient = new elasticsearch.Client({
-    hosts: [ envProps.elasticHost + ':' + envProps.elasticPort]
+  // hosts: [ envProps.elasticHost + ':' + envProps.elasticPort]
+  hosts: [envProps.elasticHost],
 });
 
-const TODO_SEARCH_INDEX_NAME = "todos";
-const TODO_SEARCH_INDEX_TYPE = "todo";
+const TODO_SEARCH_INDEX_NAME = 'todos';
+const TODO_SEARCH_INDEX_TYPE = 'todo';
 
 // Ping the client to be sure Elastic is up
-elasticClient.ping({
+elasticClient.ping(
+  {
     requestTimeout: 30000,
-}, function(error) {
+  },
+  function (error) {
     if (error) {
-        console.error('Something went wrong with Elasticsearch: ' + error);
+      console.error('Something went wrong with Elasticsearch: ' + error);
     } else {
-        console.log('Elasticsearch client connected');
+      console.log('Elasticsearch client connected');
 
-        // Check if todo index already exists?
-        const todoIndexExists = elasticClient.indices.exists({
-            index: TODO_SEARCH_INDEX_NAME
-        }, function (error, response, status) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Todo index exists in Elasticsearch');
-            }
-        });
-
-
-        if (!todoIndexExists) {
-            // Create a Todos index. If the index has already been created, then this function fails safely
-            elasticClient.indices.create({
-                index: TODO_SEARCH_INDEX_NAME
-            }, function (error, response, status) {
-                if (error) {
-                    console.log('Could not create Todo index in Elasticsearch: ' + error);
-                } else {
-                    console.log('Created Todo index in Elasticsearch');
-                }
-            });
+      // Check if todo index already exists?
+      const todoIndexExists = elasticClient.indices.exists(
+        {
+          index: TODO_SEARCH_INDEX_NAME,
+        },
+        function (error, response, status) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Todo index exists in Elasticsearch');
+          }
         }
+      );
+
+      if (!todoIndexExists) {
+        // Create a Todos index. If the index has already been created, then this function fails safely
+        elasticClient.indices.create(
+          {
+            index: TODO_SEARCH_INDEX_NAME,
+          },
+          function (error, response, status) {
+            if (error) {
+              console.log(
+                'Could not create Todo index in Elasticsearch: ' + error
+              );
+            } else {
+              console.log('Created Todo index in Elasticsearch');
+            }
+          }
+        );
+      }
     }
-});
+  }
+);
 
 // Messaging Processing ////////////////////////////////////////////////////////////////////////////////////////////////
 
-rabbitmq.connect(envProps.rabbitmqUrl, function(err, connection) {
-    connection.createChannel(function(err, channel) {
-        const searchIngestionQueue = 'search-ingestion';
+rabbitmq.connect(envProps.rabbitmqUrl, function (err, connection) {
+  connection.createChannel(function (err, channel) {
+    const searchIngestionQueue = 'search-ingestion';
 
-        channel.assertQueue(searchIngestionQueue, {durable: true});
+    channel.assertQueue(searchIngestionQueue, { durable: true });
 
-        // Get one (1) message, let other search servers grab messages if there are more
-        channel.prefetch(1);
+    // Get one (1) message, let other search servers grab messages if there are more
+    channel.prefetch(1);
 
-        console.log("Waiting for messages in '%s' queue...", searchIngestionQueue);
+    console.log("Waiting for messages in '%s' queue...", searchIngestionQueue);
 
-        channel.consume(searchIngestionQueue, function(msg) {
-            const todoTitle = msg.content.toString();
-            console.log("Received '%s' todo", todoTitle);
+    channel.consume(
+      searchIngestionQueue,
+      function (msg) {
+        const todoTitle = msg.content.toString();
+        console.log("Received '%s' todo", todoTitle);
 
-            // Update the search index
-            elasticClient.index({
-                index: TODO_SEARCH_INDEX_NAME,
-                type: TODO_SEARCH_INDEX_TYPE,
-                body: {
-                    todotext: todoTitle
-                }
-            }, function(err, resp, status) {
-                if (err) {
-                    console.log('Could not index ' + todoTitle + ": " + err);
-                } else {
-                    console.log('Added Todo: [' + todoTitle + '] to Elasticsearch Index');
-                }
-            });
+        // Update the search index
+        elasticClient.index(
+          {
+            index: TODO_SEARCH_INDEX_NAME,
+            type: TODO_SEARCH_INDEX_TYPE,
+            body: {
+              todotext: todoTitle,
+            },
+          },
+          function (err, resp, status) {
+            if (err) {
+              console.log('Could not index ' + todoTitle + ': ' + err);
+            } else {
+              console.log(
+                'Added Todo: [' + todoTitle + '] to Elasticsearch Index'
+              );
+            }
+          }
+        );
 
-            setTimeout(function() {
-                channel.ack(msg);
-            }, 1000); // 1 second
-
-        }, {noAck: false});
-    });
+        setTimeout(function () {
+          channel.ack(msg);
+        }, 1000); // 1 second
+      },
+      { noAck: false }
+    );
+  });
 });
--- todo-search-service/server.js
+++ todo-search-service/server.js
@@ -12,62 +12,71 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 // Elasticsearch Client Setup //////////////////////////////////////////////////////////////////////////////////////////
 const elasticClient = new elasticsearch.Client({
-    hosts: [ envProps.elasticHost + ':' + envProps.elasticPort]
+  // hosts: [ envProps.elasticHost + ':' + envProps.elasticPort]
+  hosts: [envProps.elasticHost],
 });
 // Ping the client to be sure Elastic is up
-elasticClient.ping({
+elasticClient.ping(
+  {
     requestTimeout: 30000,
-}, function(error) {
+  },
+  function (error) {
     if (error) {
-        console.error('Something went wrong with Elasticsearch: ' + error);
+      console.error('Something went wrong with Elasticsearch: ' + error);
     } else {
-        console.log('Elasticsearch client connected');
+      console.log('Elasticsearch client connected');
     }
-});
+  }
+);
 
 // Set up the API routes ///////////////////////////////////////////////////////////////////////////////////////////////
 
-const TODO_SEARCH_INDEX_NAME = "todos";
-const TODO_SEARCH_INDEX_TYPE = "todo";
+const TODO_SEARCH_INDEX_NAME = 'todos';
+const TODO_SEARCH_INDEX_TYPE = 'todo';
 
 // Search all todos
 app.route('/api/v1/search').post((req, res) => {
-    const searchText = req.body.searchText;
+  const searchText = req.body.searchText;
 
-    console.log('CALLED POST api/v1/search with searchText=' + searchText);
+  console.log('CALLED POST api/v1/search with searchText=' + searchText);
 
-    // Perform the actual search passing in the index, the search query and the type
-    elasticClient.search({
-        index: TODO_SEARCH_INDEX_NAME,
-        type: TODO_SEARCH_INDEX_TYPE,
-        body: {
-            query: {
-                match: {
-                    todotext: searchText
-                }
-            }
-        }
+  // Perform the actual search passing in the index, the search query and the type
+  elasticClient
+    .search({
+      index: TODO_SEARCH_INDEX_NAME,
+      type: TODO_SEARCH_INDEX_TYPE,
+      body: {
+        query: {
+          match: {
+            todotext: searchText,
+          },
+        },
+      },
     })
-    .then(results => {
-        console.log('Search for "' + searchText + '" matched (' + results.hits.hits.length + ')');
-        res.send(results.hits.hits);
+    .then((results) => {
+      console.log(
+        'Search for "' +
+          searchText +
+          '" matched (' +
+          results.hits.hits.length +
+          ')'
+      );
+      res.send(results.hits.hits);
     })
-    .catch(err => {
-        console.log(err);
-        res.send([]);
+    .catch((err) => {
+      console.log(err);
+      res.send([]);
     });
 });
 
-
 // Start the server ////////////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(port, () => {
-    console.log('Todo Search Service started!');
+  console.log('Todo Search Service started!');
 });
-
